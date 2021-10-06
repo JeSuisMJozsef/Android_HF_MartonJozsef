a8e6798c26217225448c21eda5f5e0dbd99db3f8
“JeSuisJocco” <martonjozsef@uni.sapientia.ro>
2021-10-06 15:42:01 +0300

    Hf2 masadik feladat

---
+++ Hf2/masadikfeladat/app/src/main/java/com/example/activitystatesavetwo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.activitystatesavetwo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        EditText modifiedbox=findViewById(R.id.editTextBox);
+        outState.putString("edittextvalue",modifiedbox.getText().toString());
+        CheckBox checkBox=findViewById(R.id.checkBox);
+        outState.putBoolean("checkboxvalue",checkBox.isChecked());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        TextView boxcontent=findViewById(R.id.editTextBox);
+        CheckBox checkBox=findViewById(R.id.checkBox);
+        if (savedInstanceState.getString("edittextvalue")!=null){
+            boxcontent.setText(savedInstanceState.getString("edittextvalue"));
+        }
+        if(savedInstanceState.getBoolean("checkboxvalue")){
+            checkBox.setChecked(savedInstanceState.getBoolean("checkboxvalue"));
+        }
+    }
+
+}
---
+++ push.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git push https://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@github.com/JeSuisMJozsef/Android_HF_MartonJozsef
