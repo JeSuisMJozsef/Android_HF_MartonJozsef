5769bd644c8334213a6843813fe12c9e81e34679
“JeSuisJocco” <martonjozsef@uni.sapientia.ro>
2021-12-13 15:34:33 +0200

    HF6

--- HF6/app/src/main/java/com/example/currencyfragment/ListFragment.java
+++ HF6/app/src/main/java/com/example/currencyfragment/ListFragment.java
@@ -1,64 +1,61 @@
 package com.example.currencyfragment;
 
-import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListFragment newInstance(String param1, String param2) {
-        ListFragment fragment = new ListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
+    protected static final String FRAGDET = "2";
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View vw=inflater.inflate(R.layout.fragment_list, container, false);
+
+        String[] shortNameArray={"EUR","USD","GBP","AUD","CAD","CHF","DKK","HUF"};
+        String[] longNameArray={"Euro","Dolar american","Lira sterlina","Dolar australian","Dolar canadian","Franc elvetian","Coroana Daneza","Forint"};
+        Double[] buyPriceArray={4.4100,3.9750,6.1250,2.9600,3.0950,4.2300,0.5850,0.0136};
+        Double[] sellPriceArray={4.5500,4.1450,6.3550,3.0600,3.2650,4.3300,0.6150,0.0146};
+        Integer[] imageArray={
+                R.drawable.eur,
+                R.drawable.usa,
+                R.drawable.uk,
+                R.drawable.aud,
+                R.drawable.can,
+                R.drawable.chf,
+                R.drawable.dkk,
+                R.drawable.huf,
+        };
+        ListView currList = (ListView) vw.findViewById(R.id.curr_list);
+        CurrencyListViewAdapter currAdapter=new CurrencyListViewAdapter(getActivity(),imageArray,shortNameArray,longNameArray,buyPriceArray,sellPriceArray);
+        currList.setAdapter(currAdapter);
+        final FragmentManager fragmentManager=getFragmentManager();
+        currList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (fragmentManager.findFragmentByTag(FRAGDET)!=null){
+                    TextView selectedcurr=(TextView) getActivity().findViewById(R.id.selectedcurr);
+                    selectedcurr.setText(shortNameArray[i]);
+                    TextView selectedcurrvalue=(TextView) getActivity().findViewById(R.id.selectedcurrvalue);
+                    selectedcurrvalue.setText(Double.toString(buyPriceArray[i]));
+                }
+                else{
+                    Intent intent=new Intent(getActivity().getApplicationContext(),ShowItem.class);
+                    intent.putExtra("curr",shortNameArray[i]);
+                    intent.putExtra("currvalue",Double.toString(buyPriceArray[i]));
+                    startActivity(intent);
+                }
+            }
+        });
+        return vw;
     }
 }
--- HF6/app/src/main/java/com/example/currencyfragment/MainActivity.java
+++ HF6/app/src/main/java/com/example/currencyfragment/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.currencyfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private static final String FRAGLIST = "1";
+    private static final String FRAGDET = "2";
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager=getFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            fragmentTransaction.add(R.id.listfrag,new ListFragment(),FRAGLIST);
+            fragmentTransaction.add(R.id.detfrag, new DetailsFragment(), FRAGDET);
+        }
+        else{
+            if (fragmentManager.findFragmentByTag(FRAGDET) != null){
+                fragmentTransaction.remove(fragmentManager.findFragmentByTag(FRAGDET));
+            }
+            fragmentTransaction.add(R.id.listfrag,new ListFragment(),FRAGLIST);
+        }
+        fragmentTransaction.commit();
     }
 }
